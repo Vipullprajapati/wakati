3975adc3401c25646f65c4f283f35986856f4d45
Advait Parab <advait0012@proton.me>
2025-01-20 06:52:48 +0000

    add rate limiting

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,18 +1,40 @@
-import { OpenAPIHono } from '@hono/zod-openapi'
-import { notFound, onError } from "stoker/middlewares"
-import logger from "../middleware/logger"
-
-
-export function createRouter(){
-    return new OpenAPIHono({ strict: false })
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
+import logger from "../middleware/logger";
+import { cloudflareRateLimiter } from "@hono-rate-limiter/cloudflare";
+type AppType = {
+  Variables: {
+    rateLimit: boolean;
+  };
+  Bindings: {
+    MY_RATE_LIMITER: RateLimit;
+  };
+};
+export function createRouter() {
+  type AppType = {
+    Variables: {
+      rateLimit: boolean;
+    };
+    Bindings: {
+      MY_RATE_LIMITER: RateLimit;
+    };
+  };
+  return new OpenAPIHono<AppType>({ strict: false });
 }
 
 export default function createApp() {
-    const app = createRouter()
-    app.use(logger())
-    app.notFound(notFound)
-    app.onError(onError)
-
-    return app
-
+  const app = createRouter();
+  app.use(logger());
+  app.notFound(notFound);
+  app.onError(onError);
+  app.use(
+    cloudflareRateLimiter<AppType>({
+      rateLimitBinding: (c) => c.env.MY_RATE_LIMITER,
+      keyGenerator: (c) => c.req.header("cf-connecting-ip") ?? "", // Method to generate custom identifiers for clients.
+      message: {
+        message: "Too many requests, please hold on a bit",
+      },
+    })
+  );
+  return app;
 }
